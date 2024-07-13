8180f443a976542c3eb100328a79adf6ba02fba9
robiulislam2001 <156186041+robiulislam2001@users.noreply.github.com>
2024-07-13 16:15:38 +0600

    Add files via upload

---
+++ api/babu88-reg.php
@@ -0,0 +1,81 @@
+ <?php
+
+$url = "https://feapi.1688ninja.xyz/api/member/requestCaptchaCode?captcha_id=6f736b71-8188-4615-bef5-f5a83646d4a8&captcha_code=0000";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+
+
+
+
+// Do not echo the response from url1
+
+
+// Second Request
+$url2 = "https://feapi.1688ninja.xyz/api/member";
+$phn1 = $_GET["phone"];
+$phn = ltrim($phn1, '0');
+$curl2 = curl_init($url2);
+curl_setopt($curl2, CURLOPT_URL, $url2);
+curl_setopt($curl2, CURLOPT_POST, true);
+curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
+
+$headers2 = array(
+    "Content-Type: application/json",
+     "user-agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows; U; Windows NT 6.2; x64; en-US Trident/5.0)",
+);
+curl_setopt($curl2, CURLOPT_HTTPHEADER, $headers2);
+
+$data = array(
+    "membercode" => "a" . $phn,
+    "password" => $phn,
+    "currency" => "BDT",
+    "email" => "",
+    "registration_site" => "desktop",
+    "mobile" => $phn,
+    "line" => "",
+    "referral_code" => "",
+    "is_early_bird" => "0",
+    "domain" => "https://www.babu88.co",
+    "language" => "bd",
+    "reg_type" => 2,
+    "agent_team" => "",
+    "utm_source" => null,
+    "utm_medium" => null,
+    "utm_campaign" => null,
+    "s2" => null,
+    "fp" => "444dbac87a' . $phn . 'b5132f89dcf11d1676727a",
+    "c_id" => null,
+    "pid" => null,
+    "stag" => null,
+    "tracking_url" => null,
+    "captcha_id" => "6f736b71-8188-4615-bef5-f5a83646d4a8",
+    "captcha_code" => "0000"
+);
+
+// Convert array to JSON
+$postData = json_encode($data);
+
+curl_setopt($curl2, CURLOPT_POSTFIELDS, $postData);
+
+// For debug only!
+curl_setopt($curl2, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp2 = curl_exec($curl2);
+curl_close($curl2);
+echo "Response from babu88: ";
+echo $resp2;
---
+++ api/khela88-forget.php
@@ -0,0 +1,63 @@
+   <?php
+
+
+$url = "https://feapi.iceage888.xyz/api/member/requestCaptchaCode?captcha_id=6f736b71-8188-1357-bef5-f5a83646d4a8&captcha_code=3352";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+
+
+
+//Babu88
+$phn1 = $_GET["phone"];
+$phn = ltrim($phn1, '0');
+
+$url2 = "https://feapi.iceage888.xyz/api/member/reqFgtPsw";
+
+// Request to url2
+$curl2 = curl_init($url2);
+curl_setopt($curl2, CURLOPT_URL, $url2);
+curl_setopt($curl2, CURLOPT_POST, true);
+curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+    "Content-Type: application/json", // Set content type to json
+);
+curl_setopt($curl2, CURLOPT_HTTPHEADER, $headers);
+
+$data = array(
+    'mobile' => $phn,
+    'prefix' => '+880',
+    'captcha_id' => '6f736b71-8188-1357-bef5-f5a83646d4a8',
+    'captcha_code' => '3352'
+);
+
+// Convert array to json
+$postData = json_encode($data);;
+
+curl_setopt($curl2, CURLOPT_POSTFIELDS, $postData);
+
+// For debug only!
+curl_setopt($curl2, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp2 = curl_exec($curl2);
+curl_close($curl2);
+
+echo "Response from Khela88: ";
+echo $resp2;
+?>
